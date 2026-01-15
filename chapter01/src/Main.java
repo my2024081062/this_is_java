de1233f5babbfa888cc49652dd33783e3a34c444
my2024081062 <ish0817@naver.com>
2026-01-15 11:02:54 +0900

    Update Main.java

--- chapter01/src/Main.java
+++ chapter01/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
+    /**
+     * api document 생성 하는 주석 실험1
+     */
+    public static String staticName = "정적 이름";
     public static void main(String[] args){
+
+        for(String arg : args){
+            System.out.println(arg); //Program arguments에 저장된 문자들이 배열로 저장
+        }
+        /**
+         * api document 생성 하는 주석 실험2
+         */
+        String nonStaticName = "멤버 이름";
+
+        System.out.println(staticName);
+        System.out.println(nonStaticName);
+    }
+    public void useFunction(){
+        staticWelcomes();
+        nonStaticWelcomes();
+    }
+
+    /**
+     * api document 생성 하는 주석 실험3
+     */
+    public static void staticWelcomes(){
+        System.out.println("hello world!");
+    }
+    /**
+     * api document 생성 하는 주석 실험4
+     */
+    public void nonStaticWelcomes(){
         System.out.println("hello world!");
     }
 }
