3666d840a7e82387793e975228d1dd0fbc1dc00c
my2024081062 <ish0817@naver.com>
2026-01-16 15:08:35 +0900

    gradle로 ch03 시작

---
+++ chapter03/src/main/java/ch03/sec01/Chapter03.java
@@ -0,0 +1,90 @@
+package ch03.sec01;
+
+public class Chapter03 {
+    public void third01(){
+        int x = -100;
+        x = -x;
+        System.out.println("x: " + x);
+
+        byte b = 100;
+        int y = -b;
+        System.out.println("y: " + y);
+        System.out.println("=============================");
+
+        x = 10;
+        y = 10;
+        int z;
+
+        x++;
+        ++x;
+        System.out.println("x = " + x);
+        System.out.println("=============================");
+
+        y--;
+        --y;
+        System.out.println("y = " + y);
+        System.out.println("=============================");
+
+        z = x++;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+        System.out.println("=============================");
+
+        z = ++x;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+        System.out.println("=============================");
+
+        z = ++x + y++;
+        System.out.println("z = " + z);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("=============================");
+    }
+
+    public void third02(){
+        byte v1 = 10;
+        byte v2 = 4;
+        int v3 = 5;
+        long v4 = 10L;
+
+        int result1 = v1 + v2;
+        System.out.println("result1: " + result1);
+        long result2 = v1 + v2 - v4;
+        System.out.println("result2: " + result2);
+        double result3 = (double) v1 / v2;
+        System.out.println("result3: " + result3);
+        int result4 = v1 % v2;
+        System.out.println("result4: " + result4);
+        System.out.println("=============================");
+    }
+
+    public void third03(){
+        byte var1 = 125;
+        for(int i = 0; i < 5;i++){
+            var1++;
+            System.out.println("var1: " + var1);
+        }
+        System.out.println("=============================");
+
+        byte var2 = -125;
+        for(int i = 0; i < 5;i++){
+            var2--;
+            System.out.println("var2: " + var2);
+        }
+        System.out.println("=============================");
+    }
+
+    public void third04(){
+        int apple = 1;
+        double pieceUnit = 0.1;
+        int number = 7;
+        double result1 = apple - number*pieceUnit;
+        System.out.println("사과 1개에서 남은 양" + result1);
+
+        int totalPieces = apple * 10;
+        int result2 = totalPieces - number;
+        System.out.println("10조각에서 남은 조각: " + result2);
+        System.out.println("사과 1개에서 남은 양: " + result2/10.0);
+    }
+}
---
+++ chapter03/src/main/java/com/mjc813/Main.java
@@ -0,0 +1,24 @@
+package com.mjc813;
+import ch03.sec01.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+
+        Chapter03 ch03 = new Chapter03();
+
+        ch03.third01();
+        ch03.third02();
+        ch03.third03();
+        ch03.third04();
+    }
+}
