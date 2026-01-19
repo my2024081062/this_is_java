8c703332940b988d8b58f624250000eaa9030fb0
my2024081062 <ish0817@naver.com>
2026-01-19 13:56:13 +0900

    ch03 과제2 끝

--- chapter03/src/main/java/ch03/sec01/Equation.java
+++ chapter03/src/main/java/ch03/sec01/Equation.java
@@ -4,4 +4,52 @@ public class Equation {
     public double equationFirst(int a, int b){
         return (double) -b / a ;
     }
+    public void task(){
+        int x = 10;
+        int y = 20;
+        int z = (++x) + (y--);
+        System.out.println("z = " + z);
+        System.out.println("=============================");
+
+        int score = 85;
+        String result = !(score>90) ? "가" : "나";
+        System.out.println(result);
+        System.out.println("=============================");
+
+        int pencils = 534;
+        int student = 30;
+
+        int pencilsPerStudent = pencils / student;
+        int pencilsLeft = pencils % student;
+        System.out.println("pencilsPerStudent = " + pencilsPerStudent);
+        System.out.println("pencilsLeft = " + pencilsLeft);
+        System.out.println("=============================");
+
+        int value = 356;
+        System.out.println("value = " + value/100);
+        System.out.println("=============================");
+
+        int widthTop = 5;
+        int widthBottom = 10;
+        int height = 7;
+        double area = (widthTop + widthBottom) / 2.0 * height;
+        System.out.println("area = " + area);
+        System.out.println("=============================");
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a > 7 && b <= 5);
+        System.out.println(a % 3 == 2 || b % 2 != 1);
+        System.out.println("=============================");
+
+        double n = 5.0;
+        double m = 0.0;
+        if(m != 0.0){
+            double o = n/m + 10.0;
+            System.out.println("result = " + o);
+        }else{
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+        System.out.println("=============================");
+    }
 }
--- chapter03/src/main/java/com/mjc813/Main.java
+++ chapter03/src/main/java/com/mjc813/Main.java
@@ -16,25 +16,26 @@ public class Main {
             System.out.println("i = " + i);
         }
 
-        Chapter03_1 ch03_1 = new Chapter03_1();
-        ch03_1.third01();
-        ch03_1.third02();
-        ch03_1.third03();
-        ch03_1.third04();
+//        Chapter03_1 ch03_1 = new Chapter03_1();
+//        ch03_1.third01();
+//        ch03_1.third02();
+//        ch03_1.third03();
+//        ch03_1.third04();
+//
+//        Chapter03_2 ch03_2 = new Chapter03_2();
+//        ch03_2.third06();
+//        ch03_2.third07();
+//        ch03_2.third08();
+//        ch03_2.third09();
+//        ch03_2.third10();
+//        ch03_2.third11();
+//        ch03_2.third12();
 
         Equation eq = new Equation();
         System.out.printf("2x + 4 = 0 의 해 x의 값 : %3.1f\n" , eq.equationFirst(2,4));
         System.out.println("-8x + 40 = 0 의 해 x의 값 : " +eq.equationFirst(-8,40));
         System.out.println("-5x + 32 = 0 의 해 x의 값 : " +eq.equationFirst(-5,32));
         System.out.println("=============================");
-
-        Chapter03_2 ch03_2 = new Chapter03_2();
-        ch03_2.third06();
-        ch03_2.third07();
-        ch03_2.third08();
-        ch03_2.third09();
-        ch03_2.third10();
-        ch03_2.third11();
-        ch03_2.third12();
+        eq.task();
     }
 }
