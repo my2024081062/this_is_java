df05a2d1df549bf46cfb4ae2ed7b50e30500833d
my2024081062 <ish0817@naver.com>
2026-01-21 17:08:48 +0900

    과제 내용 수정

--- chapter05/src/main/java/com/mjc813/Task03.java
+++ chapter05/src/main/java/com/mjc813/Task03.java
@@ -9,7 +9,7 @@ public class Task03 {
         int nArray[] = new int[25];
         int sum = 0;
         for (int i = 0; i< nArray.length; i++){
-            nArray[i] = (int) (Math.random()*100);
+            nArray[i] = (int) dArray[i];
             sum += nArray[i];
         }
         System.out.println("전체 원소들의 합 : " +  sum);
@@ -28,7 +28,8 @@ public class Task03 {
         int[][] nArray = new int[number1][number1];
         for(int i = 0; i < nArray.length; i++){
             for(int j = 0; j < nArray[i].length; j++){
-                System.out.print(j != nArray[i].length-1 ? j+i+1 + ", " : j+i+1 + "");
+                nArray[i][j] = i+j+1;
+                System.out.print(j != nArray[i].length-1 ? nArray[i][j] + ", " : nArray[i][j] + "");
             }
             System.out.println();
         }
@@ -39,47 +40,62 @@ public class Task03 {
             for(int j = 0; j < square[i].length; j++){
                 if(i == 0){
                     if(j == 0) {
-                        System.out.print("┌");
+                        square[i][j] = '┌';
                     }
                     else if (j == square[i].length-1){
-                        System.out.print("┐");
+                        square[i][j] = '┐';
                     }
                     else {
-                        System.out.print("─");
+                        square[i][j] = '─';
                     }
                 }
                 else if (i == square.length-1){
                     if(j == 0) {
-                        System.out.print("└");
+                        square[i][j] = '└';
                     }
                     else if (j == square[i].length-1){
-                        System.out.print("┘");
+                        square[i][j] = '┘';
                     }
                     else {
-                        System.out.print("─");
+                        square[i][j] = '─';
                     }
                 }
                 else {
                     if(j == 0 || j == square[i].length-1) {
-                        System.out.print("│");
+                        square[i][j] = '│';
                     }
                     else {
-                        System.out.print(" ");
+                        square[i][j] = ' ';
                     }
                 }
             }
+        }
+        for(int i = 0; i < square.length; i++){
+            for(int j = 0; j < square[i].length; j++){
+                System.out.print(square[i][j]);
+            }
             System.out.println();
         }
+
         char[][] chDiamond = new char[5][5];
         for(int i = 0; i < chDiamond.length; i++){
-            for(int j = 0; j < Math.abs(i-chDiamond.length/2); j++){
-                System.out.print(" ");
+            int front = Math.abs(i-chDiamond.length/2);
+            int star = chDiamond.length - Math.abs(i-chDiamond.length/2)*2;
+            for(int j = 0; j < front; j++){
+                chDiamond[i][j] = ' ';
             }
-            for(int j = 0; j < chDiamond.length - Math.abs(i-chDiamond.length/2)*2; j++){
-                System.out.print("*");
+            for(int j = front; j < front + star; j++){
+                chDiamond[i][j] = '*';
             }
-            for(int j = 0; j < Math.abs(i-chDiamond.length/2); j++){
-                System.out.print(" ");
+            for(int j = front + star; j < chDiamond.length; j++){
+                chDiamond[i][j] = ' ';
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i < chDiamond.length; i++){
+            for(int j = 0; j < chDiamond[i].length; j++){
+                System.out.print(chDiamond[i][j]);
             }
             System.out.println();
         }
