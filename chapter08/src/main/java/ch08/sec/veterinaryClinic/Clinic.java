7997ca148be2c90c3777752a8b1b96654ed03276
my2024081062 <ish0817@naver.com>
2026-02-03 09:26:59 +0900

    Create Clinic.java

---
+++ chapter08/src/main/java/ch08/sec/veterinaryClinic/Clinic.java
@@ -0,0 +1,94 @@
+package ch08.sec.veterinaryClinic;
+
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+public class Clinic {
+    private final static Clinic clinic = new Clinic();
+    private petChart[] petCharts = new petChart[0];
+    private Veterinarian[] veterinarians = new Veterinarian[0];
+    private Nurse[] nurses = new Nurse[0];
+
+    private void setPetCharts(petChart[] petCharts) {
+        this.petCharts = petCharts;
+    }
+
+    private void setVeterinarians(Veterinarian[] veterinarians) {
+        this.veterinarians = veterinarians;
+    }
+
+    private void setNurses(Nurse[] nurses) {
+        this.nurses = nurses;
+    }
+
+    private Clinic(){
+    }
+    public static Clinic getInstance(){
+        return Clinic.clinic;
+    }
+
+    public void addNerse(Nurse n){
+        Nurse[] addNerses = new Nurse[this.getNurses().length+1];
+        System.arraycopy(this.getNurses(),0,addNerses,0,this.getNurses().length);
+        addNerses[this.getNurses().length] = n;
+        this.setNurses(addNerses);
+    }
+
+    public void addVeterinarian(Veterinarian v){
+        Veterinarian[] addVeterinarians = new Veterinarian[this.getVeterinarians().length+1];
+        System.arraycopy(this.getVeterinarians(),0,addVeterinarians,0,this.getVeterinarians().length);
+        addVeterinarians[this.getVeterinarians().length] = v;
+        this.setVeterinarians(addVeterinarians);
+    }
+
+    public void addAnimalChart(petChart ac){
+        petChart[] addPetCharts = new petChart[this.getPetCharts().length+1];
+        System.arraycopy(this.getPetCharts(),0, addPetCharts,0,this.getPetCharts().length);
+        addPetCharts[this.getPetCharts().length] = ac;
+        this.setPetCharts(addPetCharts);
+    }
+
+    public void nurses_job0816(Nurse n){
+        Pet[] pets = new Pet[this.getPetCharts().length];
+        for (int i = 0; i < pets.length; i++) {
+            pets[i] = this.getPetCharts()[i].getPet(); // 필드 추출
+        }
+        while (true){ //실험용으로 true
+            if(LocalTime.now() == LocalTime.of(8,0,0) || true){
+                for(Pet pet : pets){
+                    System.out.println(pet.getName() + "에게 아침 약을 먹입니다.");
+                }
+            }
+            if(LocalTime.now() == LocalTime.of(13,0,0) || true){
+                for(Pet pet : pets){
+                    System.out.println(pet.getName() + "에게 점심 약을 먹입니다.");
+                }
+            }
+            if(LocalTime.now() == LocalTime.of(20,0,0) || true){
+                System.out.println(n.getName() + "간호사가 퇴근합니다.");
+                this.nurses_job1624(this.getNurses()[1]);
+            }
+            break;
+        }
+    }
+    public void nurses_job1624(Nurse n){
+        Pet[] pets = new Pet[this.getPetCharts().length];
+        for (int i = 0; i < pets.length; i++) {
+            pets[i] = this.getPetCharts()[i].getPet(); // 필드 추출
+        }
+        while (true){
+            if(LocalTime.now() == LocalTime.of(18,0,0) || true){
+                for(Pet pet : pets){
+                    System.out.println(pet.getName() + "에게 저녁 약을 먹입니다.");
+                }
+            }
+            if(LocalTime.now() == LocalTime.of(20,0,0) || true){
+                System.out.println(n.getName() + "간호사가 퇴근합니다.");
+                break;
+            }
+            break;
+        }
+    }
+}
