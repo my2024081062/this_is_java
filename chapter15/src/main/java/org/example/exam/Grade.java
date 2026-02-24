91066af7b96115f48e1182633b692fe61d328158
my2024081062 <ish0817@naver.com>
2026-02-24 17:18:37 +0900

    JSON CRUD 완성

---
+++ chapter15/src/main/java/org/example/exam/Grade.java
@@ -0,0 +1,7 @@
+package org.example.exam;
+
+public enum Grade {
+    ALL,
+    AGE12_OVER,
+    AGE15_OVER,
+}
---
+++ chapter15/src/main/java/org/example/exam/InputNintendoGame.java
@@ -0,0 +1,22 @@
+package org.example.exam;
+
+public class InputNintendoGame {
+    public void work(){
+        NintendoGame nintendoGame1 = new NintendoGame("마리오1",Grade.ALL,10000);
+        NintendoGame nintendoGame2 = new NintendoGame("마리오2",Grade.ALL,15000);
+        NintendoGame nintendoGame3 = new NintendoGame("마리오3",Grade.ALL,20000);
+        Myclass<NintendoGame> myclass = new Myclass<>();
+        myclass.add(nintendoGame1);
+        myclass.add(nintendoGame2);
+        myclass.add(nintendoGame3);
+        System.out.println(myclass.getJsonAllItems());
+
+        System.out.println("myclass.set(1,new NintendoGame(\"별의 커비\",Grade.ALL,13000))");
+        myclass.set(1,new NintendoGame("별의 커비",Grade.ALL,13000));
+        System.out.println(myclass.getJsonAllItems());
+
+        System.out.println("myclass.remove(1)");
+        myclass.remove(1);
+        System.out.println(myclass.getJsonAllItems());
+    }
+}
