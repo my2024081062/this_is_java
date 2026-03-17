72c13b35b3c92a68a3b05165033968684d08faee
my2024081062 <ish0817@naver.com>
2026-03-17 20:11:52 +0900

    쓸데없는 클래스가 들어갔음

--- chapter14/src/main/java/org/example/Main.java
+++ chapter14/src/main/java/org/example/Main.java
@@ -37,9 +37,6 @@ public class Main {
 //        //6
 //        PrintGugudan printGugudan = new PrintGugudan();
 //        printGugudan.printGugudan();
-        Solution solution = new Solution();
-        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        solution.solution(nums,"left");
     }
 
     private static void doThread1(){
