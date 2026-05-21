4443bf58d78684685f0a00d3f35e38d6b2ad95af
my2024081062 <ish0817@naver.com>
2026-05-21 21:11:19 +0900

    common 완성

--- login_cookie/src/main/java/com/mjc813/login_cookie/common/ComResponseDto.java
+++ login_cookie/src/main/java/com/mjc813/login_cookie/common/ComResponseDto.java
@@ -1,4 +1,22 @@
 package com.mjc813.login_cookie.common;
 
-public class ComResponseDto {
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ComResponseDto<T> {
+    private ResponseCode responseCode;
+    private String message;
+    private T result;
+
+    public static <T> ComResponseDto<T> make(ResponseCode responseCode, String message, T data) {
+        return ComResponseDto.<T>builder()
+                .responseCode(responseCode)
+                .message(message)
+                .result(data)
+                .build();
+    }
 }
--- login_cookie/src/main/java/com/mjc813/login_cookie/common/ResponseCode.java
+++ login_cookie/src/main/java/com/mjc813/login_cookie/common/ResponseCode.java
@@ -1,12 +1,19 @@
 package com.mjc813.login_cookie.common;
 
 public enum ResponseCode {
-	SUCCESS,
-	INSERT_ERROR,
-	UPDATE_ERROR,
-	DELETE_ERROR,
-	SELECT_ERROR,
-	DATA_NOT_FOUND_ERROR,
-	AUTHENTICATION_ERROR, // 로그인 인증 에러
-	AUTHORIZATION_ERROR, // 자원을 사용하기 위한 권한 인가 에러
+	SUCCESS(10000),
+	INSERT_ERROR(41000),
+	UPDATE_ERROR(42000),
+	DELETE_ERROR(43000),
+	SELECT_ERROR(44000),
+	SERVER_ERROR(49000),
+	DATA_NOT_FOUND_ERROR(51000),
+	AUTHENTICATION_ERROR(52000), // 로그인 인증 에러
+	AUTHORIZATION_ERROR(53000); // 자원을 사용하기 위한 권한 인가 에러
+
+	private Integer code;
+
+	ResponseCode(Integer code) {
+		this.code = code;
+	}
 }
