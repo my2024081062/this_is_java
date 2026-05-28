b31249d6b59a6cb0d6503c68ac90fcdecb783eb9
my2024081062 <ish0817@naver.com>
2026-05-28 18:15:37 +0900

    Enum형이라 썼는데 오히려 오류가 남

--- login_cookie/src/main/java/com/mjc813/login_cookie/models/member/MemberEntity.java
+++ login_cookie/src/main/java/com/mjc813/login_cookie/models/member/MemberEntity.java
@@ -32,7 +32,6 @@ public class MemberEntity implements IMember{
     @Column(name = "valid_text", length = 100,  nullable = false)
     private String validText;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "role", length = 10, nullable = false)
     private Role role;
 
