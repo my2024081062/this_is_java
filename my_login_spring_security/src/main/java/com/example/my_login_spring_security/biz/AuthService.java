63354d0404352d22da05575d256ecb724500b1dd
my2024081062 <ish0817@naver.com>
2026-06-05 21:48:48 +0900

    기말공부함

--- my_login_spring_security/src/main/java/com/example/my_login_spring_security/biz/AuthService.java
+++ my_login_spring_security/src/main/java/com/example/my_login_spring_security/biz/AuthService.java
@@ -7,7 +7,6 @@ import com.example.my_login_spring_security.models.member.MemberEntity;
 import com.example.my_login_spring_security.models.member.MemberJpaRepository;
 import com.example.my_login_spring_security.models.role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
