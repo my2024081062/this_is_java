2ba900f145dfdd9e5ca742590f4ed89903e8242f
my2024081062 <ish0817@naver.com>
2026-06-18 17:55:48 +0900

    Revert "Delete AuthFilter.java"
    This reverts commit a3673efb2b67e6a7561513f880698f7d9e934ca3.

---
+++ my_login_spring_security/src/main/java/com/example/my_login_spring_security/config/AuthFilter.java
@@ -0,0 +1,53 @@
+package com.example.my_login_spring_security.config;
+
+import com.example.my_login_spring_security.models.member.IMember;
+import com.example.my_login_spring_security.models.member.MemberService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request
+            , HttpServletResponse response
+            , FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if(request.getMethod().equals("OPTIONS")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            Object object = request.getSession().getAttribute("MJC_LOGIN");
+            if(object == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String signId = (String) object;
+            IMember signedMember = memberService.findBySignId(signId);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    signedMember, signedMember,signedMember.getAuthorities()
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        catch (Exception e) {
+            log.error("user auth error" + e.getMessage());
+        }
+        filterChain.doFilter(request,response);
+    }
+}
