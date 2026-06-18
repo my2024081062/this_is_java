7c86941923c7a0a41a57df1a7722d461696f7fe9
my2024081062 <ish0817@naver.com>
2026-06-18 20:17:13 +0900

    필터 설정도 바꿔봄

--- session/src/main/java/com/example/session/conf/LSHAuthenticationFilter.java
+++ session/src/main/java/com/example/session/conf/LSHAuthenticationFilter.java
@@ -8,14 +8,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class LSHAuthenticationFilter extends OncePerRequestFilter {
 
@@ -26,17 +29,25 @@ public class LSHAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request
             , HttpServletResponse response
             , FilterChain filterChain) throws ServletException, IOException {
-        filterChain.doFilter(request, response);
-        HttpSession session = request.getSession();
-        Object mjcLogin = session.getAttribute("MJC_LOGIN");
-        if(mjcLogin instanceof IMember signedMember){
-            MemberDto find = memberService.findBySignId(signedMember.getSignId());
-            UsernamePasswordAuthenticationToken auth = new  UsernamePasswordAuthenticationToken(
-                    find
-                    , null
-                    , find.getAuthorities()
-            );
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        try {
+            if(request.getMethod().equals("OPTIONS")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            HttpSession session = request.getSession();
+            Object mjcLogin = session.getAttribute("MJC_LOGIN");
+            if(mjcLogin instanceof IMember signedMember){
+                MemberDto find = memberService.findBySignId(signedMember.getSignId());
+                UsernamePasswordAuthenticationToken auth = new  UsernamePasswordAuthenticationToken(
+                        find
+                        , find
+                        , find.getAuthorities()
+                );
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            log.error("user auth error" + e.getMessage());
         }
         filterChain.doFilter(request, response);
     }
