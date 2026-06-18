3fce51991534eb17ae1bc28ad251fe2160f9a44f
my2024081062 <ish0817@naver.com>
2026-06-18 20:15:28 +0900

    웹 보안 config에는 @Configuration @EnableWebSecurity @EnableMethodSecurity 가 있어야한다.

--- session/src/main/java/com/example/session/conf/LSHWebSecurityConfig.java
+++ session/src/main/java/com/example/session/conf/LSHWebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +23,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class LSHWebSecurityConfig{
     @Autowired
     private LSHPasswordEncoder lshEncoder;
