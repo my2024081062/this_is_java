c9d2413e636817a6eb8c60a80fe5dcf8c2fcb744
my2024081062 <ish0817@naver.com>
2026-06-18 20:07:17 +0900

    mic_login인 세션가진 request마다 model.addAttribute하는 것을 지움

--- session/src/main/java/com/example/session/biz/SessionControllerAdvice.java
+++ session/src/main/java/com/example/session/biz/SessionControllerAdvice.java
@@ -1,28 +1,28 @@
-package com.example.session.biz;
-
-import com.example.session.model.member.IMember;
-import com.example.session.model.member.MemberDto;
-import com.example.session.model.member.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-@ControllerAdvice
-public class SessionControllerAdvice {
-    @Autowired
-    private MemberService memberService;
-
-    @ModelAttribute
-    public void addSignedUser(Model model
-            , @SessionAttribute(name = "MJC_LOGIN", required = false) IMember signMember ) {
-        // 해당 메소드는 클라이언트의 요청을 가로채서 쿠키의 키가 "MJC_LOGIN" 인 값을 signId 변수에 담는다.
-        if ( signMember != null ) {
-            // signedMember 가 존재한다면 member 테이블을 조회 한다.
-            MemberDto signedMember = memberService.findBySignId(signMember.getSignId());
-            model.addAttribute("signedMember", signedMember);
-            // 조회한 사용자 데이터를 model 에 추가한다. 추가할때 키 를 "signedMember" 를 줬다. 값은 MemberDto 이다.
-        }
-    }
-}
+//package com.example.session.biz;
+//
+//import com.example.session.model.member.IMember;
+//import com.example.session.model.member.MemberDto;
+//import com.example.session.model.member.MemberService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.SessionAttribute;
+//
+//@ControllerAdvice
+//public class SessionControllerAdvice {
+//    @Autowired
+//    private MemberService memberService;
+//
+//    @ModelAttribute
+//    public void addSignedUser(Model model
+//            , @SessionAttribute(name = "MJC_LOGIN", required = false) IMember signMember ) {
+//        // 해당 메소드는 클라이언트의 요청을 가로채서 쿠키의 키가 "MJC_LOGIN" 인 값을 signId 변수에 담는다.
+//        if ( signMember != null ) {
+//            // signedMember 가 존재한다면 member 테이블을 조회 한다.
+//            MemberDto signedMember = memberService.findBySignId(signMember.getSignId());
+//            model.addAttribute("signedMember", signedMember);
+//            // 조회한 사용자 데이터를 model 에 추가한다. 추가할때 키 를 "signedMember" 를 줬다. 값은 MemberDto 이다.
+//        }
+//    }
+//}
