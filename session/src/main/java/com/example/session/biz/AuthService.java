4d1972c255c6f3d8efd733e4a818874f63b3128c
my2024081062 <ish0817@naver.com>
2026-06-18 21:10:14 +0900

    security 사용, 비번 검사도 pe사용

--- session/src/main/java/com/example/session/biz/AuthService.java
+++ session/src/main/java/com/example/session/biz/AuthService.java
@@ -1,11 +1,11 @@
 package com.example.session.biz;
 
-import com.example.session.common.LoginException;
 import com.example.session.model.auth.SignInDto;
 import com.example.session.model.member.MemberEntity;
 import com.example.session.model.member.MemberJpaRepository;
 import com.example.session.model.member.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +13,16 @@ public class AuthService {
 	@Autowired
 	private MemberJpaRepository memberJpaRepository;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public Boolean signMember(SignInDto signInDto) {
 		MemberEntity find = this.memberJpaRepository.findBySignId(signInDto.getSignId()).orElseThrow();
 //		if ( !find.getValidEmail() ) {
 //			throw new LoginException("not valid email");
 //		}
 		if(signInDto.getPassword() != null) {
-			if ( signInDto.getPassword().equals(find.getPassword()) && find.getRole() != Role.GUEST ) {
+			if ( this.passwordEncoder.matches(signInDto.getPassword(), find.getPassword()) && find.getRole() != Role.GUEST ) {
 				return true;
 			}
 		}
